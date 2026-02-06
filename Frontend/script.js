0658ffd1e3644d9065fb58eb8b8da777d2584753
Yashraj Jyoti Sanjay Kastode <168764161+yashrajkastode@users.noreply.github.com>
2026-02-06 09:22:14 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- CONFIGURATION ---
-    const BACKEND_URL = 'http://127.0.0.1:8000';
+    const BACKEND_URL = 'https://med-matrix-sooo.onrender.com';
 
     // --- STATE VARIABLES ---
     let allDrugsList = [];
